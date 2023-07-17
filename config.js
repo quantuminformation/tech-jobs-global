0d0bd6aaf20e046b1731cf8a9a98daa8bd79274e
Nikos Katsikanis <nkatsikanis@gmail.com>
2023-07-17 16:47:35 +0300

    config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Stored in /config.js
 
-const BASE_URL = window.location.hostname === 'localhost' ? '' : '/vanillajs-patterns';
+const BASE_URL = window.location.hostname === 'localhost' ? '' : '/tech-jobs-global';
 
 const numberFormat = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -10,7 +10,6 @@ const numberFormat = new Intl.NumberFormat('en-US', {
 });
 
 export const config = {
-    API_KEY_GOOGLE_MAPS: 'AIzaSyCIS-VZfUUfWRqQWJfIiP8-SW2-EGiVYKs',
     BASE_URL: BASE_URL,
     DISCORD_URL: 'https://discord.gg/mgjjxDAHsf',
     MIN_ANNUAL_SALARY: numberFormat.format(50000),
